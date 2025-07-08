7b27d5c84af253aa82d1a53b8105583563b0f70e
tarek6522 <tarek652@gmail.com>
2025-07-08 11:18:39 -0700

    تمكين Whitenoise لخدمة static files من staticfiles

--- config/wsgi.py
+++ config/wsgi.py
@@ -4,3 +4,8 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
 application = get_wsgi_application()
+
+# ✅ تمكين whitenoise لخدمة staticfiles مباشرة
+from whitenoise import WhiteNoise
+application = WhiteNoise(application, root=os.path.join(BASE_DIR, 'staticfiles'))
+
