e70bf801fbc1193b00e2237db336a8d399744bf0
tarek6522 <tarek652@gmail.com>
2025-07-07 23:49:27 -0700

    تنفيذ migrate تلقائي من wsgi مؤقتًا

--- config/wsgi.py
+++ config/wsgi.py
@@ -4,3 +4,12 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
 application = get_wsgi_application()
+
+import django
+from django.core.management import call_command
+
+django.setup()
+try:
+    call_command('migrate')
+except Exception as e:
+    print(f"❌ Migration error: {e}")
