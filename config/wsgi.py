7f9ebecdfbe12283440a08947db21c60434db0f1
tarek6522 <tarek652@gmail.com>
2025-07-08 10:38:20 -0700

    تجهيز static files للعمل على Render

--- config/wsgi.py
+++ config/wsgi.py
@@ -4,8 +4,3 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
 application = get_wsgi_application()
-
-from django.contrib.auth.models import User
-
-if not User.objects.filter(username='admin').exists():
-    User.objects.create_superuser('admin', 'admin@example.com', 'adminpassword123')
